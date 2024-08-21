74021b0f81f26f09e6636fac362f07fc4eaebb4b
leinad_nofiled <102634472+DanielPCV2020@users.noreply.github.com>
2024-08-20 23:00:59 -0400

    parte1

---
+++ ocasiao/java/funcao.js
@@ -0,0 +1,49 @@
+
+/*function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  } 
+  function abrirCard (){
+  
+      let dots = document.getElementById("dots");
+      let moreText = document.getElementById("more");
+      let btnText = document.getElementsByClassName("btn-read-more");
+    
+      if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Detalhes";
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Menos";
+        moreText.style.display = "inline";
+      }  
+ }
+ */
+ function abrirCard(btn) {
+  var card = btn.closest('.Card-1'); // Encontrar o card específico
+  var dots = card.querySelector(".dots");
+  var moreText = card.querySelector(".more");
+  var btnText = card.querySelector(".btn-read-more");
+
+  if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.textContent = "Detalhes";
+      moreText.style.display = "none";
+  } else {
+      dots.style.display = "none";
+      btnText.textContent = "Ver menos";
+      moreText.style.display = "inline";
+  }
+}
