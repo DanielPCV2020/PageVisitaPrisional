8d9334b66df6e3044d9e5e451a1c0c4674f524a8
leinad_nofiled <102634472+DanielPCV2020@users.noreply.github.com>
2024-08-21 00:05:12 -0400

    menu button

--- ocasiao/java/funcao.js
+++ ocasiao/java/funcao.js
@@ -47,3 +47,11 @@
       moreText.style.display = "inline";
   }
 }
+function toggleMenu() {
+  var navLinks = document.getElementById("navLinks");
+  if (navLinks.classList.contains("show")) {
+      navLinks.classList.remove("show");
+  } else {
+      navLinks.classList.add("show");
+  }
+}
