e3f3906275d08109eb49fb6d2f353171007bec45
leinad_nofiled <102634472+DanielPCV2020@users.noreply.github.com>
2024-08-22 19:17:33 -0400

    Terminando

--- ocasiao/java/funcao.js
+++ ocasiao/java/funcao.js
@@ -17,3 +17,32 @@ function toggleMenu() {
   var navLinks = document.getElementById('navLinks');
   navLinks.classList.toggle('active');
 }  
+function animateArrow() {
+  const arrow = document.querySelector('.arrow');
+  arrow.classList.add('animate');
+
+  setTimeout(() => {
+      arrow.classList.remove('animate');
+  }, 300); // Duração da animação
+}
+window.onload = function() {
+  Swal.fire({
+      title: 'Atenção!',
+      text: 'Todos os itens estão sujeitos a uma revista pela autoridade competente.',
+      icon: 'info',
+      confirmButtonText: 'Entendido',
+      confirmButtonColor: '#d33',
+      background: '#f7f7f7',
+      color: '#333',
+      padding: '2rem',
+  });
+};
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
