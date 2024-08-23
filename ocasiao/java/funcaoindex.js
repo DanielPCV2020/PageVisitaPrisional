fa0bffaba46304b70f47008b718aed3c09fdba71
leinad_nofiled <102634472+DanielPCV2020@users.noreply.github.com>
2024-08-23 16:48:28 -0400

    ok

--- ocasiao/java/funcaoindex.js
+++ ocasiao/java/funcaoindex.js
@@ -14,3 +14,10 @@ $(document.body).on('click', 'a[href*="#"]', function(e) {
       });
     }
   });
+  document.addEventListener("DOMContentLoaded", function() {
+    const h3Element = document.getElementById("portaria");
+    const link = "https://drive.google.com/file/d/1Au9vt3Hy8dM5rhYGuHY14EBwLc557xzs/view?usp=drivesdk";
+    const linkText = "4366/2023-SEJUS";
+    
+    h3Element.innerHTML = `Guia de itens permitido pela portaria <a href="${link}" target="_blank">${linkText}</a>`;
+  });
